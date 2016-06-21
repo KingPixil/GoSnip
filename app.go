e4b7f5b715f14bf80fccb8f066af06aa1fd8a9eb
ilivethego <lsdfsgdf@gmail.com>
2016-06-21 19:59:44 +0000

    no templating

--- app.go
+++ app.go
@@ -14,8 +14,7 @@ var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func main() {
     i := http.FileServer(http.Dir("static"))
-    http.Handle("/static/", http.StripPrefix("/static/", i))
-    http.HandleFunc("/", serveTemplate)
+    http.Handle("/", i))
 	http.HandleFunc("/new", n)
 
 	log.Println("Snip is Listening")
@@ -44,37 +43,3 @@ func randCode(n int) string {
     return string(b)
 }
 
-/* Template serving */
-func serveTemplate(w http.ResponseWriter, r *http.Request) {
-  lp := path.Join("static", "index.html")
-  fp := path.Join("static", r.URL.Path)
-
-  // Return a 404 if the template doesn't exist
-  info, err := os.Stat(fp)
-  if err != nil {
-    if os.IsNotExist(err) {
-      http.NotFound(w, r)
-      return
-    }
-  }
-
-  // Return a 404 if the request is for a directory
-  if info.IsDir() {
-    http.NotFound(w, r)
-    return
-  }
-
-  tmpl, err := template.ParseFiles(lp, fp)
-  if err != nil {
-    // Log the detailed error
-    log.Println(err.Error())
-    // Return a generic "Internal Server Error" message
-    http.Error(w, http.StatusText(500), 500)
-    return
-  }
-
-  if err := tmpl.ExecuteTemplate(w, "layout", nil); err != nil {
-    log.Println(err.Error())
-    http.Error(w, http.StatusText(500), 500)
-  }
-}
