7f2e400d47a092495d85bc31fe9d17f1d3362af8
kingsofcoding <simplecooldude1@gmail.com>
2016-07-09 23:54:57 +0000

    basic file server

--- main.go
+++ main.go
@@ -4,4 +4,20 @@ import (
     "github.com/KingPixil/straw"
     "string"
     "net/http"
+    "os"
 )
+
+func main() {
+    port := os.Getenv("PORT")
+    
+    http.HandleFunc("/", landing)
+    http.HandleFunc("/new", newHandler)
+}
+
+func landing(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "./static/index.html")
+}
+
+func newHandler(w http.ResponseWriter, r *http.Request) {
+    
+}
