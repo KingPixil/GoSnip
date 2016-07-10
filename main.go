847d36a3a22bf2a7c7696a6200527f5ad3459dc3
kingsofcoding <simplecooldude1@gmail.com>
2016-07-10 00:06:34 +0000

    listen and serve

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
     "github.com/KingPixil/straw"
-    "string"
     "net/http"
     "os"
 )
@@ -19,6 +18,8 @@ func main() {
     
     http.HandleFunc("/", landing)
     http.HandleFunc("/new", newHandler)
+    
+    http.ListenAndServe(":" + port, nil)
 }
 
 func landing(w http.ResponseWriter, r *http.Request) {
