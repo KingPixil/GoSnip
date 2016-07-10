e17b2951d2d5c28501abb3157633b3bd6c26b366
kingsofcoding <simplecooldude1@gmail.com>
2016-07-10 00:02:37 +0000

    util

--- main.go
+++ main.go
@@ -7,9 +7,16 @@ import (
     "os"
 )
 
+var code string
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
 func main() {
     port := os.Getenv("PORT")
-    public := http.FileServer(http.Dir("./static/public"))
+    public := http.FileServer(http.Dir("./static/public/"))
+    
+    
+    http.Handle("/assets", public)
+    
     http.HandleFunc("/", landing)
     http.HandleFunc("/new", newHandler)
 }
@@ -19,5 +26,20 @@ func landing(w http.ResponseWriter, r *http.Request) {
 }
 
 func newHandler(w http.ResponseWriter, r *http.Request) {
-    
+    if r.Method == "POST" {
+        url := r.FormValue("url")
+        
+    }
+}
+
+func redir(w http.ResponseWriter, r *http.Request) {
+    http.Redirect(w, r, url, 301)
+}
+
+func randCode(n int) string {
+    b := make([]rune, n)
+    for i := range b {
+        b[i] = letters[rand.Intn(len(letters))]
+    }
+    return string(b)
 }
